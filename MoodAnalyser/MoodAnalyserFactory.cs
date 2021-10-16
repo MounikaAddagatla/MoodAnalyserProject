529983137eb5a05b84afd13a639c53e422aa9557
MounikaAddagatla <89588852+MounikaAddagatla@users.noreply.github.com>
2021-10-16 11:00:39 +0530

    Add: Invoke a method for reflection for throw exception method not found

--- MoodAnalyser/MoodAnalyserFactory.cs
+++ MoodAnalyser/MoodAnalyserFactory.cs
@@ -37,5 +37,53 @@ namespace MoodAnalyser
                 throw new CustomException(CustomException.ExceptionType.NO_SUCH_METHOD, "Constructor not found");
             }
         }
+        public static object ParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = Type.GetType(className);
+            try
+            {
+                if (type.FullName.Equals(className) || type.Name.Equals(className))
+                {
+                    if (type.Name.Equals(constructorName))
+                    {
+                        ConstructorInfo info = type.GetConstructor(new[] { typeof(string) });
+                        object instance = info.Invoke(new object[] { message });
+                        return instance;
+                    }
+                    //if class and constructor name is not equal then it throws constructor not found exception
+                    else
+                    {
+                        throw new CustomException(CustomException.ExceptionType.NO_SUCH_METHOD, "Constructor not found");
+                    }
+
+                }
+                //if class is not found then it throws class not found exception
+                else
+                {
+                    throw new CustomException(CustomException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex;
+            }
+        }
+
+        public static string InvokeAnalyseMood(string message, string methodName)
+        {
+            try
+            {
+                Type type = Type.GetType("MoodAnalyzer.MoodAnalyser");
+                object moodAnalyseObject = MoodAnalyserFactory.ParameterizedConstructor("MoodAnalyzer.MoodAnalyser", "MoodAnalyser", message);
+                MethodInfo analyseMoodInfo = type.GetMethod(methodName);
+                object mood = analyseMoodInfo.Invoke(moodAnalyseObject, null);
+                return mood.ToString();
+            }
+            catch (NullReferenceException)
+            {
+                throw new CustomException(CustomException.ExceptionType.NO_SUCH_METHOD, "Method is Not Found");
+            }
+        }
     }
+    
 }
--- MoodAnalyserTest/MoodAnalyserUnitTest.cs
+++ MoodAnalyserTest/MoodAnalyserUnitTest.cs
@@ -40,5 +40,92 @@ namespace MoodAnalyserTest
                 Assert.AreEqual(expected, ex.Message);
             }
         }
+        [Test]
+        public void GivenMoodAnalyser_ShouldReturnMoodAnalyserObject()
+        {
+            object expected = new Analyser("Happy");
+            object actual = MoodAnalyserFactory.ParameterizedConstructor("MoodAnalyser.Analyser", "Analyser", "Happy");
+            expected.Equals(actual);
+        }
+
+        [Test]
+
+        public void GivenMoodAnalyserWrongClassName_ShouldThrowMoodAnalysisException()
+        {
+            string expected = "Class not found";
+            try
+            {
+                object actual = MoodAnalyserFactory.ParameterizedConstructor("MoodAnalyser.Analyser", "MoodAnalyser", "Happy");
+            }
+            catch (CustomException ex)
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
+        }
+
+        [Test]
+
+        public void GivenMoodAnalyserClassNameWithNoProperConstructor_ShouldThrowMoodAnalysisException()
+        {
+            string expected = "Constructor not found";
+            try
+            {
+                object actual = MoodAnalyserFactory.ParameterizedConstructor("MoodAnalyser.Analyser", "Analyser", "I am happy");
+            }
+            catch (CustomException ex)
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
+        }
+        [Test]
+        public void Given_MoodAnalyser_Class_Name_Should_Return_MoodAnalyser_Object_Using_Parametrized_Constructor()
+        {
+            //Arrange
+            string className = "MoodAnalyzer.MoodAnalyser";
+            string constructor = "MoodAnalyser";
+            Analyser expectedObj = new Analyser("HAPPY");
+            //Act
+            object resultObj = MoodAnalyserFactory.ParameterizedConstructor(className, constructor, "HAPPY");
+            //Assert
+            expectedObj.Equals(resultObj);
+        }
+        //TestCase-6.2
+        [Test]
+        public void Given_Wrong_Class_Name_Should_Throw_MoodAnalysisException_For_Parameterized_Constructor()
+        {
+            try
+            {
+                //Arrange
+                string className = "WrongNameSpace.MoodAnalyser";
+                string constructorName = "MoodAnalyser";
+                Analyser expectedObj = new Analyser("HAPPY");
+                //Act
+                object resultObj = MoodAnalyserFactory.ParameterizedConstructor(className, constructorName, "HAPPY");
+            }
+            catch (CustomException e)
+            {
+                //Assert
+                Assert.AreEqual("Class Not Found", e.Message);
+            }
+        }
+        //TC 6.3:-Pass Wrong Constructor parameter, cactch the Exception and throw indicating No such method Error
+        [Test]
+        public void Given_Improper_Constructor_Name_Should_Throw_MoodAnalysisException_For_Parameterized_Constructor()
+        {
+            try
+            {
+                //Arrange
+                string className = "MoodAnalyzer.MoodAnalyser";
+                string constructorName = "WrongConstructorName";
+                Analyser expectedObj = new Analyser("HAPPY");
+                //Act
+                object resultObj = MoodAnalyserFactory.ParameterizedConstructor(className, constructorName, "HAPPY");
+            }
+            catch (CustomException e)
+            {
+                //Assert
+                Assert.AreEqual("Constructor is not Found", e.Message);
+            }
+        }
     }
 }
